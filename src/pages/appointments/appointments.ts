6b547d9b08d1259313932cdea5a71fa5275eab5d
amishal1 <humanabstract1.1@gmail.com>
2018-04-17 22:01:39 +0500

    Third commit

--- src/pages/appointments/appointments.ts
+++ src/pages/appointments/appointments.ts
@@ -8,10 +8,32 @@ import { NewAppointmentPage } from '../new-appointment/new-appointment';
 })
 export class AppointmentsPage {
 
+  public docName;
+  public appDate;
   constructor(public navCtrl: NavController) {
   }
   goToNewAppointment(params){
     if (!params) params = {};
     this.navCtrl.push(NewAppointmentPage);
   }
+
+ 
+
+  ngAfterViewInit() {
+    let testingVar ;
+    testingVar = localStorage.getItem('appointment');
+
+    testingVar =  JSON.parse(testingVar);
+
+    console.log(testingVar.docname);
+
+    this.docName = testingVar.docname;
+    this.appDate = testingVar.appdate;
+  }
+
+  
+  
+  
+
+
 }
--- src/pages/new-appointment/new-appointment.ts
+++ src/pages/new-appointment/new-appointment.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild, AfterViewInit,
   ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+//import { VariableAst } from '@angular/compiler';
 
 
 
@@ -16,19 +17,36 @@ export class NewAppointmentPage implements AfterViewInit {
  
 
 
-    @ViewChild('someInput') someInput: ElementRef;
+    //@ViewChild('someInput') someInput: ElementRef;
     @ViewChild('testingInput') testingInput: ElementRef;
+    @ViewChild('doctorName') doctorName: ElementRef;
+    @ViewChild('appointmentDate') appointmentDate: ElementRef;
+   // @ViewChild('notification') note: ElementRef;
   
     ngAfterViewInit() {
-      this.someInput.nativeElement.value = "Anchovies! 🍕🍕";
+
+      //this.someInput.nativeElement.value = "Anchovies! 🍕🍕";
     }
 
     addNewPill(){
+
       let testingVar;
-      testingVar = this.testingInput.nativeElement.value;
-      alert(testingVar);
-      localStorage.setItem('testingKey', testingVar);
+      //testingVar = this.testingInput.nativeElement.value;
+      //alert(testingVar);
+      //localStorage.setItem('testingKey', testingVar);
       
+      let docNameVar = this.doctorName.nativeElement.value;
+      let appDateVar = this.appointmentDate.nativeElement.value;
+      console.log(docNameVar);
+      console.log(appDateVar);
+      //let noteVar = this.note.nativeElement.value;
+
+    var appointmentObj = {docname:docNameVar, appdate:appDateVar};
+  
+
+      localStorage.setItem('appointment', JSON.stringify(appointmentObj) );
+      console.log(JSON.stringify(appointmentObj));
+
     }
   
 
